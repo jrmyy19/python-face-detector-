26f6caebd16ad972650af307ed2c515b69ed1c58
jeremy <jeremybourgeois191@icloud.com>
2026-07-27 20:50:18 -0400

    tuto

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
-def main():
-    print("Hello, World!")
-    for i in range(12):
-        print(i)
-main()
+import cv2 as cv
+img= cv.imread("senegal-flag.jpg")
+def rescaleImage(frame,scale=0.75):
+    width=int(frame.shape[1]*scale)
+    height =int(frame.shape[0]*scale)
+    dimensions= (width,height)
+    return cv.resize(frame,dimensions,interpolation=cv.INTER_AREA)
+ 
+
+rescaledPhoto=rescaleImage(img)
+cv.imshow("senegal",rescaledPhoto)
+
+capture= cv.VideoCapture("video.mp4")
+while True:
+    isTrue, frame = capture.read()
+    frame_resized=rescaleImage(frame,scale=0.1)
+
+    cv.imshow("video", frame)
+    cv.imshow("video resized",frame_resized)
+    if cv.waitKey(20) & 0xFF==ord("d"):
+        break
+capture.release()
+cv.destroyAllWindows 
