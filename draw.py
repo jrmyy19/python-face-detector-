62ad44ddfb8e202bfdfe1c5cbb1fca9c7932dfbb
jeremy <jeremybourgeois191@icloud.com>
2026-07-27 21:46:02 -0400

    learn how to write text

--- draw.py
+++ draw.py
@@ -10,9 +10,12 @@ red=0,0,255
 #blank[200:300,300:400]=blue
 #cv.imshow("color",blank)
 # draw a rectangle
-cv.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),green,thickness=-1)
-cv.imshow("rectangle",blank)
+#cv.rectangle(blank,(0,0),(blank.shape[1]//2,blank.shape[0]//2),green,thickness=-1)
+#cv.imshow("rectangle",blank)
 #draw a circle
-cv.circle(blank,(250,250),40,red,-1)
-cv.imshow("circle",blank)
+#cv.circle(blank,(250,250),40,red,-1)
+#cv.imshow("circle",blank)
+#write text 
+cv.putText(blank,"hello",(225,225),cv.FONT_HERSHEY_TRIPLEX,1.0,green,2)
+cv.imshow("text",blank)
 cv.waitKey(0) 
